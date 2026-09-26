14cc5c70afdee6c43bbe6c907eefef38a8826bf4
krs83 <krs83@yandex.ru>
2026-09-26 10:30:00 +0300

    fix: colors codes

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-Color kRedSection = Color(0xFF153fa1);
-Color kBlueSection = Color(0xFFc92236);
+Color kRedSection = Color(0xFFc92236);
+Color kBlueSection = Color(0xFF153fa1);
 Color kYellowTimer = Color(0xFFfbbb15);
 Color kAppBackGroundColor = Color(0xFF15262b);
